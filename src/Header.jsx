13fb0f10fbc7256c15d3bfed69b4ba89d068d9ef
Parth Seth <parthseth2004@gmail.com>
2023-05-06 01:13:29 +0530

    noob header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './Header.css';
+
 function Header(){
     return(
+        <>
+        
         <div className="head">
             <ul className="nav">
-                <li> Home </li>
-                <li> About </li>
-                <li> Our Mission </li>
+                <li > 
+                    <a href="/"> 
+                    <img src='https://upload.wikimedia.org/wikipedia/en/thumb/4/42/GLA_University_logo.png/180px-GLA_University_logo.png' alt='Your Library Name' />
+                    </a> 
+                </li>
+                <li> 
+                    <a href="/">Home</a> 
+                </li>
+                <li> 
+                    <a href="/about">About</a> 
+                </li>
+                <li> 
+                    <a href="/mission">Our Mission</a> 
+                </li>
+                <li className="cta"> 
+                    <a href="/signin">Sign In</a> 
+                </li>
             </ul>
         </div>
+        </>
+        
     );
 }
+
 export default Header;
