2711f128907613d3df2a4b5b4d5b2ec5b12a4a38
Parth Seth <parthseth2004@gmail.com>
2023-06-09 16:20:57 +0530

    backend

---
+++ express server/server.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app = express();
+const bodyparser= require("body-parser");
+
+app.get("/" , function (request , response) {
+    response.send("Hello");
+});
+
+app.post("/")
+app.listen(3000 , function() {
+    console.log("server started");
+});
