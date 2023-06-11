567c01307776ca599bd28b95e47cb25e9530185f
Parth Seth <parthseth2004@gmail.com>
2023-06-11 21:46:05 +0530

    removed express

--- express server/server.js
+++ express server/server.js
@@ -5,7 +5,6 @@ const bodyparser= require("body-parser");
 app.get("/" , function (request , response) {
     response.send("Hello");
 });
-
 app.post("/")
 app.listen(3000 , function() {
     console.log("server started");
